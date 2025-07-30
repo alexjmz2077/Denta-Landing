00202e2a99215fc968b78f911f6a1ba30fe0dfbb
alexjmz2077 <alex.-37@hotmail.com>
2025-07-30 08:29:06 -0500

    Fix: Indexacion IMG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,9 @@ export default function RootLayout({
 
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
